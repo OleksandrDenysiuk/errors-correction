bd769e8132b70ad272300ea625906dc19057ae91
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-06 13:50:33 +0300

    Added hello page

--- src/main/java/com/portfolio/errorscorrection/controller/IndexController.java
+++ src/main/java/com/portfolio/errorscorrection/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.portfolio.errorscorrection.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-
+    @GetMapping("/")
+    public String loadPage(){
+        return "index";
+    }
 }
