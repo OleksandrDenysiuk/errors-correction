1831392abb0b09b48fde0899d6310b25ac6a4f74
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-05-08 22:42:21 +0300

    Added model attribute

--- src/main/java/com/portfolio/errorscorrection/controller/CalculateController.java
+++ src/main/java/com/portfolio/errorscorrection/controller/CalculateController.java
@@ -1,20 +1,27 @@
 package com.portfolio.errorscorrection.controller;
 
 import com.portfolio.errorscorrection.command.CrcCommand;
+import com.portfolio.errorscorrection.converter.CrcCommandToCrc;
 import com.portfolio.errorscorrection.service.CrcService;
+import com.portfolio.errorscorrection.service.MessageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculateController {
 
     private final CrcService crcService;
+    private final MessageService messageService;
+    private final CrcCommandToCrc crcCommandToCrc;
 
-    public CalculateController(CrcService crcService) {
+    public CalculateController(CrcService crcService, MessageService messageService, CrcCommandToCrc crcCommandToCrc) {
         this.crcService = crcService;
+        this.messageService = messageService;
+        this.crcCommandToCrc = crcCommandToCrc;
     }
 
     @GetMapping("/input")
@@ -25,7 +32,10 @@ public class CalculateController {
     }
 
     @PostMapping("/input/data/send")
-    public String calculate(@ModelAttribute(name = "crc") CrcCommand crc){
-        return "";
+    public String calculate(@RequestParam(name = "text") String text,
+                            @ModelAttribute(name = "crc") CrcCommand crc,
+                            Model model){
+        model.addAttribute("message", messageService.generate(text,crcCommandToCrc.convert(crc)));
+        return "formBrakeBits";
     }
 }
