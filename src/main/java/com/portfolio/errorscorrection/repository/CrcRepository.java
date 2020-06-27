67d32287b1e89afffbad445b4a2088ed989b0e53
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-27 18:51:55 +0200

    Refactor

--- src/main/java/com/portfolio/errorscorrection/repository/CrcRepository.java
+++ src/main/java/com/portfolio/errorscorrection/repository/CrcRepository.java
@@ -1,7 +1,8 @@
 package com.portfolio.errorscorrection.repository;
+
 import com.portfolio.errorscorrection.model.Crc;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CrcRepository extends CrudRepository<Crc,Long>{
+public interface CrcRepository extends JpaRepository<Crc,Long> {
 }
