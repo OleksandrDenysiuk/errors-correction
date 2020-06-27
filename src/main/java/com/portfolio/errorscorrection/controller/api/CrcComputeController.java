a5a599ef215ae72c1cef86356835eee9bd5e1ce9
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-27 18:50:04 +0200

    Add CrcCompute rest controller

---
+++ src/main/java/com/portfolio/errorscorrection/controller/api/CrcComputeController.java
@@ -0,0 +1,29 @@
+package com.portfolio.errorscorrection.controller.api;
+
+import com.portfolio.errorscorrection.converter.CrcDtoToCrc;
+import com.portfolio.errorscorrection.model.Crc;
+import com.portfolio.errorscorrection.service.CrcComputeService;
+import com.portfolio.errorscorrection.service.CrcService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CrcComputeController {
+    private final CrcComputeService crcComputeService;
+    private final CrcService crcService;
+    private final CrcDtoToCrc crcDtoToCrc;
+
+    public CrcComputeController(CrcComputeService crcComputeService, CrcService crcService, CrcDtoToCrc crcDtoToCrc) {
+        this.crcComputeService = crcComputeService;
+        this.crcService = crcService;
+        this.crcDtoToCrc = crcDtoToCrc;
+    }
+
+    @PostMapping("/crc/{crcId}/compute")
+    public @ResponseBody
+    int compute(@RequestParam String message,
+                   @PathVariable Long crcId) {
+        Crc crc = crcDtoToCrc.convert(crcService.getOneById(crcId));
+        return crcComputeService.compute(message.getBytes(), crc);
+    }
+}
