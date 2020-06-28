0e050144305c67278092eb8013ef1f2a1184ad69
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-29 00:46:27 +0200

    Bit simulation controller

---
+++ src/main/java/com/portfolio/errorscorrection/controller/api/BitSimulationController.java
@@ -0,0 +1,38 @@
+package com.portfolio.errorscorrection.controller.api;
+
+import com.portfolio.errorscorrection.model.Bit;
+import com.portfolio.errorscorrection.service.BitService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BitSimulationController {
+    private final BitService bitService;
+
+    public BitSimulationController(BitService bitService) {
+        this.bitService = bitService;
+    }
+
+    @PostMapping("/bit/fix")
+    public @ResponseBody
+    List<Bit> fixBit(List<Bit> bitList, int position){
+        return bitService.fixBit(bitList, position);
+    }
+
+    @PostMapping("/bit/fix")
+    public @ResponseBody
+    List<Bit> brakeBit(List<Bit> bitList, List<Integer> positions){
+        return bitService.brakeBit(bitList, positions);
+    }
+
+    @PostMapping("/bit/fix")
+    public @ResponseBody
+    List<Bit> generateBitList(String input){
+        return bitService.generateBitList(input);
+    }
+}
